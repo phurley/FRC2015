ec5a826f3d2c71a45c5d7858123cded997301172
Nate Schickler <nate@schickler.org>
2015-01-23 21:01:22 -0800

    added check for soft limit in telescope, added initialize imu in drivetrain

--- src/org/usfirst/frc/team294/robot/subsystems/Telescope.java
+++ src/org/usfirst/frc/team294/robot/subsystems/Telescope.java
@@ -28,7 +28,7 @@ public class Telescope extends Subsystem {
 	//PotLimitedSpeedController teleMotor = new PotLimitedSpeedController(telescope, telescopePot, "pivMinLimit", "pivMaxLimit");
 	//SpeedController pivotMotor=pivotMotorUnlimited;
 	
-
+	
 	
 	public Telescope() {
 		// Use these to get going:
@@ -43,8 +43,15 @@ public class Telescope extends Subsystem {
 		System.out.println("limit="+Preferences.getInstance().getDouble("pivMaxLimit", 0.0));
 		
 		((MultiCANTalon) telescope).SetInverted(1, true);
-		getMainTelescope().changeControlMode(ControlMode.Position);
+		//getMainTelescope().changeControlMode(ControlMode.Position);
 		getMainTelescope().setPID(1.0, 1.0, 1.0); //TODO
+		
+		
+		
+		
+	
+		
+		
 		
 		//setInputRange(Preferences.getInstance().getDouble("pivMinLimit", 0.0),
 		//		Preferences.getInstance().getDouble("pivMaxLimit", 5.0));
@@ -60,9 +67,35 @@ public class Telescope extends Subsystem {
 		return ((MultiCANTalon) telescope).getCANTalon(0);
 	}
 	
+	private int forwardLimit=1023;
+	private int reverseLimit=0;
+	private int voltageThreshold=5;
+	
+	public int getForwardLimit(){
+		return this.forwardLimit;
+	}
+	public int getReverseLimit(){
+		return this.reverseLimit;
+	}
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    	
+    	//if the voltage is over the threshold, sets the current position as the soft limit for the telescope
+    	if(getMainTelescope().getBusVoltage()>voltageThreshold){    		
+    		int topIsCloser=getMainTelescope().getAnalogInPosition()-forwardLimit;
+    		int bottomIsCloser=getMainTelescope().getAnalogInPosition()-reverseLimit;
+    		if(topIsCloser>bottomIsCloser){
+    			getMainTelescope().enableForwardSoftLimit(true);
+    			forwardLimit=getMainTelescope().getAnalogInPosition();
+    			getMainTelescope().setForwardSoftLimit(forwardLimit);
+    		}else{
+    			getMainTelescope().enableReverseSoftLimit(true);
+    			reverseLimit=getMainTelescope().getAnalogInPosition();
+    			getMainTelescope().setReverseSoftLimit(reverseLimit);
+    		}	
+    	}
     }
     
    // protected double returnPIDInput() {
